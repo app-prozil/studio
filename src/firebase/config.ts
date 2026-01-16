2e0dbc8f536ad162b820eacd3d9b430db56cf758
Conta Apps <contaparaplugns@gmail.com>
2026-01-16 00:39:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4723096270-1d6e0",
+  "appId": "1:290758889413:web:ef384d6be4e116776405ac",
+  "apiKey": "AIzaSyAGMJ_e7gp6jdawTZGy_S_Wqj7cUOR22QM",
+  "authDomain": "studio-4723096270-1d6e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "290758889413"
+};
