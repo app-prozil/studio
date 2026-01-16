1939677961aee681eb6443a0c2af67a3a0a3a29e
Conta Apps <contaparaplugns@gmail.com>
2026-01-16 00:44:33 +0000

    lembrando que no futuro irei hospedar o app no Vercel ok.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4723096270-1d6e0",
-  "appId": "1:290758889413:web:ef384d6be4e116776405ac",
-  "apiKey": "AIzaSyAGMJ_e7gp6jdawTZGy_S_Wqj7cUOR22QM",
-  "authDomain": "studio-4723096270-1d6e0.firebaseapp.com",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-4723096270-1d6e0",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:290758889413:web:ef384d6be4e116776405ac",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAGMJ_e7gp6jdawTZGy_S_Wqj7cUOR22QM",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-4723096270-1d6e0.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "290758889413"
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "290758889413"
 };
