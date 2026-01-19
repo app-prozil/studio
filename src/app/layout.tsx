2d2198b3d495cb3e44d2afd8c103168d94a60ccc
Conta Apps <contaparaplugns@gmail.com>
2026-01-19 18:08:51 +0000

    O ICONE E O FAVICOM, USE O MESMO ICONE DO ROBOZINHO DO NOSSO PROZIL,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { FirebaseClientProvider } from '@/firebase/client-provider';
 export const metadata: Metadata = {
   title: 'ProZil',
   description: 'Atividades escolares para crianças com baixa visão e pouca mobilidade.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -24,14 +25,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="ProZil" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#84c6c7" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#84c6c7" />
-
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="manifest" href="/manifest.json" />
-
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
