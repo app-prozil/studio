76f4513e1c6a8790d0fe202790956ae0c3b115fc
Conta Apps <contaparaplugns@gmail.com>
2026-01-19 18:05:50 +0000

    AGORA AJUSTE PARA QUE OS NAVEGADORES DETECTEM COMO PWA LEMBRANDO QUE IRE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        <meta name="application-name" content="ProZil" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="ProZil" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#84c6c7" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#84c6c7" />
+
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
