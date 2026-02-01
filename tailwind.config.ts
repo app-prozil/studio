378cc813fac13ed504f52dcf4547c7c5c5ac375c
Conta Apps <contaparaplugns@gmail.com>
2026-02-01 02:28:54 +0000

    adicionei o arquivo das font mas não foi reconhecido norretamente, poder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
         body: ['PT Sans', 'sans-serif'],
         headline: ['PT Sans', 'sans-serif'],
         code: ['monospace'],
-        libras: ['Libras Black 2020', 'sans-serif'],
+        libras: ['LibrasFont', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
