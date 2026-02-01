92a31ec4f26d0879b8e039be00385fec3ee72758
Conta Apps <contaparaplugns@gmail.com>
2026-02-01 04:46:06 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -644,10 +644,13 @@ const SidebarMenuSkeleton = React.forwardRef<
     showIcon?: boolean
   }
 >(({ className, showIcon = false, ...props }, ref) => {
-  // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+  const [width, setWidth] = React.useState("75%");
+
+  React.useEffect(() => {
+    // This will only run on the client, after the initial render.
+    setWidth(`${Math.floor(Math.random() * 40) + 50}%`);
+  }, []);
+
 
   return (
     <div
